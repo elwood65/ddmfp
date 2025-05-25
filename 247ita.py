456b211b43caee4ea84eaa256df409fb7c9932d6
elwood65 <fenotti65@gmail.com>
2025-05-25 07:53:22 -0300

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -5,8 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://elwood65-vadase.hf.space"     
+MFPPSW = "shadow65"
 
 # Costanti
 M3U8_OUTPUT_FILE = "247ita.m3u8"
